cd38a3107faaf862187e9c2001c3eefa1f546675
jakkireddymahendhrareddy <jakkireddymahendhrareddy@example.com>
2024-07-17 15:38:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const card=document.getElementById("card");
+const red=document.getElementById("red");
+const blue=document.getElementById("blue");
+const green=document.getElementById("green");
+const orange=document.getElementById("orange");
+const back=document.getElementById("back");
+
+function clickred(){
+    card.style.backgroundColor="red";
+}
+function clickblue(){
+    card.style.backgroundColor="blue";
+}
+function clickgreen(){
+    card.style.backgroundColor="green";
+}
+function clickorange(){
+    card.style.backgroundColor="orange";
+}
+function clickback(){
+    card.style.backgroundColor="black";
+}
